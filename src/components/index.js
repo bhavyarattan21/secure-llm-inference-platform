672c01ca96f987ab84cbab7a8ec0ecc9a77560a7
Lakshya Dangwal <lakshya260107@gmail.com>
2026-02-11 22:11:07 +0530

    Add index.js for component exports

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+// Component exports for easier imports
+export { default as Header } from './Header';
+export { default as DefenseToggle } from './DefenseToggle';
+export { default as AttackSidebar } from './AttackSidebar';
+export { default as Dashboard } from './Dashboard';
+export { default as StatusCard } from './StatusCard';
+export { default as StatsGrid } from './StatsGrid';
+export { default as AttackLab } from './AttackLab';
+export { default as ConsolePanel } from './ConsolePanel';
+export { default as DirectChat } from './DirectChat';
+export { default as NetworkPanel } from './NetworkPanel';
